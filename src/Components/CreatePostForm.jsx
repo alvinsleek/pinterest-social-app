703cf99472e0dd2c4c505f7fd2e4d207505b9e5a
Alvin Mithamo <alvinmithamo100@gmail.com>
2023-01-24 22:04:23 +0300

    feat:creating form and data posted to the database

--- src/Components/CreatePostForm.jsx
+++ src/Components/CreatePostForm.jsx
@@ -1,9 +1,62 @@
-import React from "react";
+import React,{useState} from "react";
 
-function CreatePostForm () {
-// Create a form that has inputs able to post name, pic,description and time 
-// must have state 
-//Post to the database 
-    return (null)
+const CreatePostForm = () =>{
+    const [formData, setFormData] = useState({
+        name:"",
+        pic:"",
+        description:"",
+        time:""
+    });
+
+    const handleChange = (e) => {
+        const {name, value } = e.target;
+        setFormData({...formData, [name]: value});
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetch('api/submit', {
+            method: 'POST',
+            body: JSON.stringify(formData),
+            headers: {'Content-type': 'application/json'}
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log('success',data);
+        })
+        
+        }
+        console.log(formData);
+    
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Name:
+                <input type="text" name="name" value=" " onChange={handleChange} />
+            </label>
+            <label><label>
+               
+                Picture:
+                <input type="image" name="picture" value=" " onChange={handleChange} />
+            </label>
+            <br />
+            <br />
+                Description:
+                <input type="text" name="description" value=" " onChange={handleChange} />
+            </label>
+            <label>
+                <br />
+                Time:
+                <input type="time" name="time" value=" " onChange={handleChange} />
+          
+           </label>
+            <br />
+            <br />
+            <button type="submit">Submit</button>
+        </form>
+    );
 };
 export default CreatePostForm
+
+
